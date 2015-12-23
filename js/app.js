8f5fb492e9b0f6c1c821103730f26d63bc61ff08
mzvast <mzvast@gmail.com>
2015-12-23 19:25:18 +0800

    Separating Out the Model

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
-var ViewModel = function() {	
+var ViewModel = function() {
+	this.currentCat = ko.observable(new Cat());	
+	this.incementCounter = function() {
+		this.currentCat().clickCount(this.currentCat().clickCount()+1);
+	};
+
+};
+
+var Cat = function() {
 	this.clickCount = ko.observable(99);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/4154543904_6e2428c421_z.jpg');
 	this.imgAttribution = '';	
-	this.incementCounter = function() {
-		this.clickCount(this.clickCount()+1);
-	};
-	this.level = ko.computed(function() {
-        return this.clickCount()>100?"Teen":
-        (this.clickCount()>10?"Infant":"Newborn");
-    },this);	
+	// this.level = ko.computed(function() {
+ //        return this.clickCount()>100?"Teen":
+ //        (this.clickCount()>10?"Infant":"Newborn");
+ //    },this);	
     this.nickname = ko.observableArray(
     	[
     		'Tabtab',
     		'fuck',
     		'dog',
     		'doggie'   		
-    	]);
+    	]);	
 };
-
 ko.applyBindings(new ViewModel())
