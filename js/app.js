e230cdeaf01a031f28164265e2e6f647cec11abb
mzvast <mzvast@gmail.com>
2015-12-23 20:05:32 +0800

    'with' and Binding Contexts

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 var ViewModel = function() {
+	var self = this;
 	this.currentCat = ko.observable(new Cat());	
 	this.incementCounter = function() {
-		this.currentCat().clickCount(this.currentCat().clickCount()+1);
+		self.currentCat().clickCount(self.currentCat().clickCount()+1);
 	};
 
 };
@@ -11,10 +12,10 @@ var Cat = function() {
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/4154543904_6e2428c421_z.jpg');
 	this.imgAttribution = '';	
-	// this.level = ko.computed(function() {
- //        return this.clickCount()>100?"Teen":
- //        (this.clickCount()>10?"Infant":"Newborn");
- //    },this);	
+	this.level = ko.computed(function() {
+        return this.clickCount()>100?"Teen":
+        (this.clickCount()>10?"Infant":"Newborn");
+    },this);	
     this.nickname = ko.observableArray(
     	[
     		'Tabtab',
