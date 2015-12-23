bcdca65558ba7326685c397f6d42d6bba1ec29b9
mzvast <mzvast@gmail.com>
2015-12-23 18:48:11 +0800

    Add nickname using observableArray foreach

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
 var ViewModel = function() {
 	var self = this;
-	this.clickCount = ko.observable(99);
-	this.name = ko.observable('Tabby');
-	this.imgSrc = ko.observable('img/4154543904_6e2428c421_z.jpg');
-	this.imgAttribution = '';	
-	this.incementCounter = function() {
-		this.clickCount(this.clickCount()+1);
+	self.clickCount = ko.observable(99);
+	self.name = ko.observable('Tabby');
+	self.imgSrc = ko.observable('img/4154543904_6e2428c421_z.jpg');
+	self.imgAttribution = '';	
+	self.incementCounter = function() {
+		self.clickCount(self.clickCount()+1);
 	};
-	this.level = ko.computed(function() {
-        return this.clickCount()>100?"Teen":
-        (this.clickCount()>10?"Infant":"Newborn");
-    }, this);	
+	self.level = ko.computed(function() {
+        return self.clickCount()>100?"Teen":
+        (self.clickCount()>10?"Infant":"Newborn");
+    });	
+    self.nickname = ko.observableArray(
+    	[
+    		{name:'Tabtab'},
+    		{name:'fuck'},
+    		{name:'dog'},
+    		{name:'doggie'}   		
+    	]);
 };
 
 ko.applyBindings(new ViewModel())
