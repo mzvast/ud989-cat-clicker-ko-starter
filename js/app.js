7c3775efc9186bf259d892ae4c2fa6b3348e593e
mzvast <mzvast@gmail.com>
2015-12-23 19:07:30 +0800

    Set self back to this

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
-var ViewModel = function() {
-	var self = this;
-	self.clickCount = ko.observable(99);
-	self.name = ko.observable('Tabby');
-	self.imgSrc = ko.observable('img/4154543904_6e2428c421_z.jpg');
-	self.imgAttribution = '';	
-	self.incementCounter = function() {
-		self.clickCount(self.clickCount()+1);
+var ViewModel = function() {	
+	this.clickCount = ko.observable(99);
+	this.name = ko.observable('Tabby');
+	this.imgSrc = ko.observable('img/4154543904_6e2428c421_z.jpg');
+	this.imgAttribution = '';	
+	this.incementCounter = function() {
+		this.clickCount(this.clickCount()+1);
 	};
-	self.level = ko.computed(function() {
-        return self.clickCount()>100?"Teen":
-        (self.clickCount()>10?"Infant":"Newborn");
-    });	
-    self.nickname = ko.observableArray(
+	this.level = ko.computed(function() {
+        return this.clickCount()>100?"Teen":
+        (this.clickCount()>10?"Infant":"Newborn");
+    },this);	
+    this.nickname = ko.observableArray(
     	[
     		'Tabtab',
     		'fuck',
