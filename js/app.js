653c97997d25aafa78834f764cc77c2871835b70
mzvast <mzvast@gmail.com>
2015-12-23 18:31:12 +0800

    Add Cat Level

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 var ViewModel = function() {
 	var self = this;
-	this.clickCount = ko.observable(9);
+	this.clickCount = ko.observable(99);
 	this.name = ko.observable('Tabby');
 	this.imgSrc = ko.observable('img/4154543904_6e2428c421_z.jpg');
 	this.imgAttribution = '';	
 	this.incementCounter = function() {
 		this.clickCount(this.clickCount()+1);
-	};	
+	};
+	this.level = ko.computed(function() {
+        return this.clickCount()>100?"Teen":
+        (this.clickCount()>10?"Infant":"Newborn");
+    }, this);	
 };
 
 ko.applyBindings(new ViewModel())
